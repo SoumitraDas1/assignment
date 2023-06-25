680549c519401d6cfc5f89acea2578afc343fad0
SoumitraDas1 <129649911+SoumitraDas1@users.noreply.github.com>
2023-06-26 02:44:35 +0530

    Update bitStuffingPython.py

--- bitStuffingPython.py
+++ bitStuffingPython.py
@@ -1,9 +1,10 @@
-a='101011111111'
-l=len(a)
+signal='101011111111'
+l=len(signal)   #counting the total bits in signal
 i=0
 while i<(l-5):
-    if a[i:i+5]=='11111':
-        a=a[:i+5]+'0'+a[i+5:]
-        i+=5
-    else: i+=1
-print(a)
+    if signal[i:i+5]=='11111':                  #if we get continuous five 1's 
+        signal=signal[:i+5]+'0'+signal[i+5:]    # then we append a '0' after that
+        i+=5                                    #and increase the i pointer to 5
+    else: i+=1      #else increase the i pointer to 1
+print(signal)
+#output 1010111110111
